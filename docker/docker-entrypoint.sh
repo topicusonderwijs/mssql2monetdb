b07ff5eb63821a360ad58c8060e5e84e9f5ed3c4
Dennis Pallett <dennis@pallett.nl>
2015-07-02 09:23:58 +0200

    Add support for switch flag in Docker image

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -6,16 +6,26 @@ if [ "$1" == "" ]; then
 	exit 1
 fi
 
+SWITCH_FLAG=""
+for ARG in "$@"
+do
+	if [ "$ARG" == "--no-switch" ] || [ "$ARG" == "--switch-only" ]; then
+		SWITCH_FLAG="$ARG"
+	fi
+done
+
 for CONFIG in "$@"
 do
-    if [ ! -f "$CONFIG" ]; then
-		echo "WARN: skipping '$CONFIG', not available in container" >&2
-	else
-		# run tool with config
-		echo "Running MSSQL2MonetDB tool with config file: $CONFIG"
-		echo "========================="
-		java -jar mssql2monetdb-$TOOL_VERSION.jar -c "$CONFIG"
-		echo "========================="
+	if [ "$CONFIG" != "--no-switch" ] && [ "$CONFIG" != "--switch-only" ]; then
+	    if [ ! -f "$CONFIG" ]; then
+			echo "WARN: skipping '$CONFIG', not available in container" >&2
+		else
+			# run tool with config
+			echo "Running MSSQL2MonetDB tool with config file: $CONFIG"
+			echo "========================="
+			java -jar mssql2monetdb-$TOOL_VERSION.jar -c "$CONFIG" $SWITCH_FLAG
+			echo "========================="
+		fi
 	fi
 done
 
