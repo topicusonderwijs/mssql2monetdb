841c05779ecb1340c0aec776b9fa2c6bdb2c56cb
Arjan van de Kamp <arjan.van.de.kamp@topicus.nl>
2017-12-14 12:36:11 +0100

    Jenkinsfile toegevoegd, en pom aangepast

---
+++ Jenkinsfile-mssql2monetdb-docker.groovy
@@ -0,0 +1,32 @@
+config {
+	daysToKeep = 21
+	cronTrigger = '@weekend'
+	disableGithubPushTrigger = true
+}
+
+node() {
+	catchError {
+		stage "Checkout"
+		
+	    git.checkout { }
+
+		stage "Maven package"
+		//Deze maven package zorgt ervoor dat de docker:build niet klaagt dat er geen 'target' dir is.
+		maven { 
+			stage = ''
+			goals = 'package'
+		}
+        
+        stage "Build docker image"
+        
+		maven { 
+			cleanup = false
+			stage = ''
+			options = 'docker:build -DpushImageTag'
+			goals = 'package'
+		}
+	}
+    notify {
+		slackChannel = "#datazoo-jenkins"
+	}
+}
