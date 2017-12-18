840606ed16ccc8e294afc01ccec60a4d15b0662f
Arjan van de Kamp <arjan.van.de.kamp@topicus.nl>
2017-12-18 09:24:11 +0100

    log4j.properties verplaatst

--- Jenkinsfile-mssql2monetdb-docker.groovy
+++ Jenkinsfile-mssql2monetdb-docker.groovy
@@ -6,24 +6,25 @@ config {
 
 node() {
 	catchError {
-		stage "Checkout"
-		
-	    git.checkout { }
+		stage("Checkout") {
+			git.checkout { }
+		}
 
-		stage "Maven package"
-		//Deze maven package zorgt ervoor dat de docker:build niet klaagt dat er geen 'target' dir is.
-		maven { 
-			stage = ''
-			goals = 'package'
+		stage("Maven package") {
+		   	//Deze maven package zorgt ervoor dat de docker:build niet klaagt dat er geen 'target' dir is.
+			maven { 
+				stage = ''
+				goals = 'package'
+			}               
 		}
         
-        stage "Build docker image"
-        
-		maven { 
-			cleanup = false
-			stage = ''
-			options = 'docker:build -DpushImageTag'
-			goals = 'package'
+    	stage("Build docker image") {
+			maven { 
+				cleanup = false
+				stage = ''
+				options = 'docker:build -DpushImageTag'
+				goals = 'package'
+			}
 		}
 	}
     notify {
